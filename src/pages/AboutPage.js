b997a72420551ba652f7c8c4b9dc654e00e30b8e
Sebastian Latkolic <latkolicsebastian@gmail.com>
2021-01-14 13:46:52 +0200

    deploy

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,19 @@
+const AboutPage = () => (
+	<>
+		<h1>About me</h1>
+		<p>
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam
+			exercitationem consectetur distinctio veniam deserunt quae vero itaque
+			debitis ipsum, consequuntur quas. Eum est corrupti adipisci, a provident
+			quos consequatur perspiciatis!
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, omnis
+			tempora delectus explicabo aliquid quibusdam repellendus tempore commodi
+			quis saepe placeat optio odio sed blanditiis? Dolore cupiditate explicabo
+			adipisci voluptatum.
+		</p>
+	</>
+);
+
+export default AboutPage;
